64373e42bf84964a715e493b60fa5a9c6b3bc893
Alexander Roiatchki <al.roiatchki@gmail.com>
2023-03-08 19:26:13 +0200

    check problematic users or items

--- reco_test.py
+++ reco_test.py
@@ -1,6 +1,8 @@
 from river import optim
 from river import reco
+from river import metrics
 import csv
+import ast
 import warnings
 warnings.filterwarnings("error")
 
@@ -17,6 +19,8 @@ dataset = (
     ({'user': 'Bob', 'item': 'Notting Hill'}, 2)
 )
 
+filter_problematic = True
+
 
 def main():
     model = reco.BiasedMF(
@@ -26,6 +30,25 @@ def main():
         latent_initializer=optim.initializers.Normal(mu=0., sigma=0.1, seed=71)
     )
 
+    metric = metrics.MAE()
+
+    problematic_users = set()
+    problematic_items = set()
+
+    if filter_problematic:
+        with open("problematic_users.txt", "r") as f:
+            ser = f.read()
+            problematic_users = \
+                set() if ser == str(set()) \
+                else ast.literal_eval(ser)
+            print(f"Problematic users recovered: {len(problematic_users)}")
+        with open("problematic_items.txt", "r") as f:
+            ser = f.read()
+            problematic_items = \
+                set() if ser == str(set()) \
+                else ast.literal_eval(ser)
+            print(f"Problematic items recoveres: {len(problematic_items)}")
+
     # Working with the 100k Twitch data from here:
     # https://cseweb.ucsd.edu/~jmcauley/datasets.html
     with open("100k_a.csv", "r") as f:
@@ -33,28 +56,48 @@ def main():
         data = list(reader)
         for data_point in data:
             # print(data_point)
-            example = ({
-                "user": data_point[0],
-                "item": data_point[1],
+            example = (
+                {
+                    "user": data_point[0],
+                    "item": data_point[2],
                 },
                 float(data_point[4]) - float(data_point[3])
-                )
+            )
             # print(example)
             # return
-            if example[1] < 1.:
-                print("Zero duration, skipping example.")
+            # if example[1] < 1. or example[1] > 50:
+            if example[0]["user"] in problematic_users or \
+                    example[0]["item"] in problematic_items:
+                print("Problematic, skipping example:")
+                print(example)
+                print(metric)
                 continue
 
             try:
+                y_pred = model.predict_one(**example[0])
                 model.learn_one(**example[0], y=example[1])
+                metric.update(example[1], y_pred)
+                # print(metric)
             except RuntimeWarning:
                 print(example)
+                problematic_users |= set([example[0]["user"]])
+                problematic_items |= set([example[0]["item"]])
 
     # for x, y in dataset:
     #     model.learn_one(**x, y=y)
 
+    print(metric)
+
     print(model.predict_one(user='Bob', item='Harry Potter'))
     print(model.predict_one(user='Putin', item='Brokeback Mountain'))
+
+    print(f"Problematic users found: {len(problematic_users)}")
+    print(f"Problematic items found: {len(problematic_items)}")
+
+    with open("problematic_users.txt", "w") as f:
+        f.write(str(problematic_users))
+    with open("problematic_items.txt", "w") as f:
+        f.write(str(problematic_items))
 
 
 if __name__ == "__main__":
