033c5ccbb7787f0682a4dfa37b9be211f427a3a9
Alexander Roiatchki <al.roiatchki@gmail.com>
2023-03-15 23:28:29 +0200

    added app routes

--- model_api.py
+++ model_api.py
@@ -4,7 +4,8 @@ from river import metrics
 from tqdm import tqdm
 import pickle
 import csv
-from flask import Flask
+from flask import Flask, request
+import json
 
 
 class BiasedRecoModel:
@@ -16,11 +17,13 @@ class BiasedRecoModel:
             latent_initializer=optim.initializers.Normal(mu=0., sigma=0.1)
         )
         self.metric = metrics.MAE()
+        self.is_trained = False
 
     def train(self, data_set):
         for data_point in tqdm(data_set):
             x, y = data_point
             self.model.learn_one(**x, y=y)
+        self.is_trained = True
 
     def save(self):
         with open("./.models/model.pkl", "wb") as f:
@@ -43,12 +46,43 @@ def load_training_data():
     return training_data
 
 
+global model
+
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
     return "IncReco model"
+
+
+@app.route("/train_model", methods=["GET"])
+def train_model():
+    print("Model training...")
+    model = BiasedRecoModel(10)
+    training_data = load_training_data()
+    model.train(training_data)
+    model.save()
+    return "Model trained and saved."
+
+
+@app.route("/load_model", methods=["GET"])
+def load_model():
+    print("Loading model...")
+    global model
+    model = BiasedRecoModel(10)
+    model.load()
+    return "Model loaded"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    global model
+    # if not model.is_trained:
+    #     return "Model not trained. Train the model first or load a trained model."
+    print(model.is_trained)
+    prediction = model.model.predict_one(**dict(request.json))
+    return f"Predicted score: {prediction}"
 
 
 if __name__ == "__main__":
